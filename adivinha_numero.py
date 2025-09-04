5eed25b0d454a8f97bc971d0f77d4868baa1e3c5
Gabriel <greis826@email.com>
2025-09-04 17:11:27 -0300

    novo arquivo

---
+++ adivinha_numero.py
@@ -0,0 +1,15 @@
+import random
+
+numero_secreto= random.randint(0,100)
+numero= int(input('digite um numero entre 0 a 100: \n'))
+
+while numero != numero_secreto:
+    print('você erro!')
+    if numero > numero_secreto:
+        print(f"o numero {numero} esta acima do numero secreto")
+    else:
+        print(f"o numero {numero} esta abaixo do numero secreto")
+
+    numero= int(input('digite um numero entre 0 a 100: \n'))
+
+print('parabens, você acerto! \n o numero secreto é {}'.format(numero_secreto))
